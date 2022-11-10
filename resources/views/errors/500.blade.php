ebc592000c9e7e45c9057f06ce7f5c3607c4837e
Nhi Uyen <fakernguyen123@gmail.com>
2022-11-11 00:51:41 +0700

    Update script src

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}?time={{ time() }}" />
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
     <title>500 - Server error</title>
 </head>
 <body class="h-screen overflow-hidden flex items-center justify-center" style="background: #edf2f7;">
