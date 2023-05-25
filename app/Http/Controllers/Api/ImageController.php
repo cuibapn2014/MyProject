a892270bc2a82f0dbc20a838430d6aee6b67f798
cuibapn2014 <fakernguyen123@gmail.com>
2023-05-25 22:26:14 +0700

    Add Image Api Controller

---
+++ app/Http/Controllers/Api/ImageController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Image;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Symfony\Component\HttpFoundation\Response;
+
+class ImageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $image = Image::find($id);
+        if(!$image) return response()->json(['msg' => 'File không tồn tại hoặc đã bị xóa'], Response::HTTP_BAD_REQUEST);
+        File::delete(public_path("img/".$image->urlImage));
+        $image->delete();
+        return response()->json(['msg' => 'Xóa thành công'], Response::HTTP_OK);
+    }
+}
