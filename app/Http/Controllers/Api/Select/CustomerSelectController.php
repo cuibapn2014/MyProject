5c64c442035bc73c2edcb49e1ddb0bd8999d738e
cuibapn2014 <fakernguyen123@gmail.com>
2023-06-05 01:56:13 +0700

    Add customer, product select box

---
+++ app/Http/Controllers/Api/Select/CustomerSelectController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Api\Select;
+
+use App\Http\Controllers\Controller;
+use App\Models\Customer;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CustomerSelectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        //
+        $customers = Customer::where('name', 'like', '%' . $request->name . '%')
+            ->orderBy('name')
+            ->get(['name AS text', 'id AS value']);
+        return response()->json(['code' => 200, 'data' => $customers], Response::HTTP_OK);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/Api/Select/ProductSelectController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Api\Select;
+
+use App\Http\Controllers\Controller;
+use App\Models\Ingredient;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProductSelectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        //
+        $products = Ingredient::where('Ten', 'like', '%' . $request->name . '%')
+            ->where('id_ingredient_type', '!=', 1)
+            ->orderBy('Ten')
+            ->get(['Ten AS text', 'id AS value']);
+        return response()->json(['code' => 200, 'data' => $products], Response::HTTP_OK);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
