c0db16a85c917f37b10692b3c7020efb0399c488
cuibapn2014 <fakernguyen123@gmail.com>
2023-07-06 01:05:42 +0700

    Update Helper

--- app/Helpers/Functions.php
+++ app/Helpers/Functions.php
@@ -3,3 +3,10 @@ function number_format_str($number)
 {
     return strrev(implode('.', str_split(strrev($number), 3)));
 }
+
+function generateCode($id, $char = "DH"){
+    date_default_timezone_set("Asia/Ho_Chi_Minh");
+    $today = date('Y-m-d');
+    $code = $char . date('d') . date('m') . date('y') . $id;
+    return $code;
+}
