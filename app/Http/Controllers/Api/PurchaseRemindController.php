39e129a843ee8716f5c248de747b7a96e0e9831b
cuibapn2014 <fakernguyen123@gmail.com>
2023-07-06 01:08:54 +0700

    Add create purchare api

--- app/Http/Controllers/Api/PurchaseRemindController.php
+++ app/Http/Controllers/Api/PurchaseRemindController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\PlanIngredient;
+use App\Models\ProductionRequest;
 use App\Models\RequestProduction;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -77,5 +79,45 @@ class PurchaseRemindController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    /**
+     * Create request purchase
+     * @param int $id_request
+     * @return \Illuminate\Http\Response
+     * */
+    public function createPurchase($id_request)
+    {
+        $planIngredient = PlanIngredient::where('id_production_request', $id_request)->get();
+        // if(count($planIngredient) <= 0) return back()->withErrors(['available' => 'Vui lòng tạo kế hoạch vật tư trước khi tạo yêu cầu mua hàng']);
+
+        $check = ProductionRequest::findOrFail($id_request);
+        if ($check->amount == $check->completed) {
+            // return back()->withErrors(['available' => 'Không thể tạo yêu cầu mua hàng cho sản phẩm đã hoàn thành']);
+            return response()->json(['msg' => 'error', 'errors' => (object)['available' => 'Không thể tạo yếu cầu mua hàng cho đề nghị sản xuất đã hoàn thành']], Response::HTTP_BAD_REQUEST);
+        }
+
+        RequestProduction::where('id_production_request', $id_request)->delete();
+        foreach ($planIngredient as $plan) {
+            if ($plan->ingredient->ingredient_type->id == 1) {
+                $requestProduction = RequestProduction::where('id_production_request', $plan->id_production_request)
+                    ->where('id_ingredient', $plan->id_ingredient);
+
+                if (!$requestProduction->exists() && $plan->total > 0) {
+                    RequestProduction::create([
+                        'id_ingredient' => $plan->id_ingredient,
+                        'id_production_request' => $plan->id_production_request,
+                        'amount' => $plan->total,
+                        'censor' => auth()->user()->id
+                    ]);
+                } else {
+                    $requestProduction = $requestProduction->first();
+                    $requestProduction->amount += $plan->total;
+                    $requestProduction->save();
+                }
+            }
+        }
+
+        return response()->json(['msg' => 'Tạo yêu cầu mua hàng thành công'], Response::HTTP_OK);
     }
 }
