d53b756ba7139329ed23e0b8d16baa38c3e0b512
Manh Tuan <fakernguyen123@gamil.com>
2024-03-24 19:05:36 +0700

    Add casts for user

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,6 +46,7 @@ class User extends Authenticatable implements MustVerifyEmail, JWTSubject
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'status' => 'integer'
     ];
 
     public function role()
